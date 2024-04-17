42993c69ec2fee39ae080137afa0f31cc1645ead
SarathLUN <lun.sarath@gmail.com>
2024-04-17 18:27:26 +0700

    finish lesson 29 game state.

--- project-game-prototype-with-rusty-engine/src/main.rs
+++ project-game-prototype-with-rusty-engine/src/main.rs
@@ -1,8 +1,22 @@
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
-struct GameState {
-    health_left: i32,
+struct GameState{
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+        }
+    }
 }
 
 fn main() {
@@ -10,5 +24,5 @@ fn main() {
 
     // get your game stuff ready here
 
-    game.run(GameState { health_left: 42 });
+    game.run(GameState::default());
 }
